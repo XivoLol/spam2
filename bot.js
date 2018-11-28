6803fd31d7926f0a0f88b53dd8679219bbb642fe
XivoLol <39168262+XivoLol@users.noreply.github.com>
2018-11-28 22:05:53 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -27,29 +27,4 @@ function timerFunc() {
 
 var timer = setTimeout(timerFunc, 1000);
 
-client.login(process.env.BOT_TOKEN);
-
-
-client.on('warn', console.warn);
-
-client.on('error', console.error);
-
-
-client.on('ready', () => console.log('ProBot Credits Miner Discord.js Script','\n','By Alphacodes','\n','ProBot credits miner is ready!'));
-
-client.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
-
-client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));
-
-
-function timerFunc() {
-    client.on('message', msg => {
-        client.guilds.get(ServerID).channels.get(ChannelID).send("آنيكك آمكك بدون تفآل يخوآت القحبةةء") 
-
-
-    });
-}
-
-var timer = setTimeout(timerFunc, 1000);
-
 client.login(process.env.BOT_TOKEN2);
