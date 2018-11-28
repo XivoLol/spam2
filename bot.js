e4d9a12c3b2163f41a45f6c7853ba37dae7f1ed8
XivoLol <39168262+XivoLol@users.noreply.github.com>
2018-11-28 14:07:03 +0000

    Update bot.js

--- bot.js
+++ bot.js
@@ -28,3 +28,34 @@ function timerFunc() {
 var timer = setTimeout(timerFunc, 1000);
 
 client.login(process.env.BOT_TOKEN);
+
+var ServerID = "517326676409974795"; //اي دي السيرفر
+var ChannelID = "517326676409974797";// اي دي الروم
+
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('warn', console.warn);
+
+client.on('error', console.error);
+
+
+client.on('ready', () => console.log('ProBot Credits Miner Discord.js Script','\n','By Alphacodes','\n','ProBot credits miner is ready!'));
+
+client.on('disconnect', () => console.log('PROBOT credits miner had disconnected!'));
+
+client.on('reconnecting', () => console.log('PROBOT credits miner is reconnecting...'));
+
+
+function timerFunc() {
+    client.on('message', msg => {
+        client.guilds.get(ServerID).channels.get(ChannelID).send("By : Xivooooooo") 
+
+
+    });
+}
+
+var timer = setTimeout(timerFunc, 1000);
+
+client.login(process.env.BOT_TOKEN2);
